601dff214c61e7301383d67eba021b27e98651f6
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-09-07 09:44:29 +0900

    formatting

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Sanctumサンプル</title>
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Sanctumサンプル</title>
+
+    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
